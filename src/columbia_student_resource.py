821458ab871cf6884315ce411775d779b4d9df0a
吴迪 <wudi@wudideMBP.lan>
2022-09-28 22:45:23 -0400

    environment setup

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -1,6 +1,6 @@
 import pymysql
 
-import os
+# import os
 
 
 class ColumbiaStudentResource:
@@ -11,10 +11,12 @@ class ColumbiaStudentResource:
     @staticmethod
     def _get_connection():
 
-        usr = os.environ.get("diwu")
-        pw = os.environ.get("dw3013")
-        h = os.environ.get("localhost")
-
+        # usr = os.environ.get("diwu")
+        # pw = os.environ.get("dw3013")
+        # h = os.environ.get("localhost")
+        usr = 'admin'
+        pw = '20176098wd'
+        h = 'hw1-dw3013.c4gryhcdew19.us-east-1.rds.amazonaws.com'
         conn = pymysql.connect(
             user=usr,
             password=pw,
